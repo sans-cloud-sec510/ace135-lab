d34830fcd4b1650afeb39d049649777cb1eb7ae0
Brandon Evans <contact@brandonmevans.com>
2024-02-26 20:11:50 -0600

    Include Lambda changes.

--- infrastructure/functions/workshop/dependency.js
+++ infrastructure/functions/workshop/dependency.js
@@ -2,26 +2,49 @@
 const AWS = require('aws-sdk')
 const axios = require('axios')
 
-const secretsManagerSecretArn = process.env.SECRETS_MANAGER_SECRET_ARN
+const s3ObjectArn = process.env.S3_OBJECT_ARN
 const pastebinId = process.env.PASTEBIN_ID
 
 module.exports = async () => {
   let payload
 
   try {
-    if (secretsManagerSecretArn) {
-      console.log(`Retrieving the payload from a secret in the AWS Secrets Manager: ${secretsManagerSecretArn}`)
+    if (s3ObjectArn) {
+      console.log(`Retrieving the payload from an S3 file: ${s3ObjectArn}`)
 
-      const secretsManager = new AWS.SecretsManager()
+      const bucketAndObject = s3ObjectArn.split(':::')[1]
+
+      if (!bucketAndObject) {
+        console.error('Invalid S3 object ARN.')
+        return
+      }
+
+      const [bucket, object] = bucketAndObject.split('/')
+
+      if (!object) {
+        console.error('Invalid S3 object ARN: object not included.')
+        return
+      }
+
+      /*
+      The region must be specified in the endpoint because enabling private DNS for a private endpoint only creates:
+      - *.s3.<Region>.amazonaws.com
+      - *.s3-accesspoint.<Region>.amazonaws.com
+      - *.s3-control.<Region>.amazonaws.com
+      */
+      const s3 = new AWS.S3({
+        endpoint: `https://s3.${process.env.AWS_REGION}.amazonaws.com`
+      })
 
       payload = await new Promise((resolve, reject) => {
-        secretsManager.getSecretValue({
-          SecretId: secretsManagerSecretArn
+        s3.makeUnauthenticatedRequest('getObject', {
+          Bucket: bucket,
+          Key: object
         }, (err, data) => {
           if (err) {
             reject(err)
           } else {
-            resolve(JSON.parse(data.SecretString).payload)
+            resolve(data.Body.toString())
           }
         })
       })
